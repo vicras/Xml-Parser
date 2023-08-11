d4f27b39472c5bc4475c3d3009352641fd0fe3c6
Viktar Hraskou <viktar.hraskou@sap.com>
2023-08-11 13:16:41 +0300

    add scheme validator

--- src/main/java/com/example/wppl/service/WpplService.java
+++ src/main/java/com/example/wppl/service/WpplService.java
@@ -4,8 +4,14 @@ import com.example.wppl.reader.impl.WppParallelReader;
 import com.example.wppl.dao.E1WPA01_Dao;
 import com.example.wppl.dao.EDI_DC40_Dao;
 import com.example.wppl.dao.SpecialQuerryDao;
+import com.example.wppl.validator.WpplFileValidator;
 import org.springframework.stereotype.Service;
+import org.xml.sax.SAXException;
 
+import javax.xml.XMLConstants;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import java.io.File;
 import java.util.List;
 
 @Service
@@ -31,5 +37,12 @@ public class WpplService {
         e1WPA01Dao.saveAll(result.e1WPA01s);
         ediDc40Dao.saveAll(result.ediDc40s);
         return specialQuerryDao.findAllDocnum();
+    }
+
+    public boolean isValidXmlFile(String filePathName, String schemaDocPath ) throws SAXException {
+        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        Schema schema = schemaFactory.newSchema(new File(schemaDocPath));
+        var fileValidator = new WpplFileValidator(schema);
+        return fileValidator.isValid(filePathName);
     }
 }
--- src/test/java/com/example/wppl/WpplServiceTests.java
+++ src/test/java/com/example/wppl/WpplServiceTests.java
@@ -1,8 +1,5 @@
 package com.example.wppl;
 
-import com.example.wppl.dto.ParseResult;
-import com.example.wppl.reader.impl.WppParallelReader;
-import com.example.wppl.reader.impl.WpplFullFileReader;
 import com.example.wppl.service.WpplService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -14,13 +11,20 @@ import org.springframework.test.context.jdbc.Sql;
 @Sql(scripts = "db-clean.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 class WpplServiceTests {
 
-    private static final String FILE_NAME = "WP_PL.xml";
+    private static final String XML_FILE_NAME = "WP_PL.xml";
+    private static final String SCHEMA_FILE_NAME = "scheme.xsd";
 
     @Autowired
     private WpplService service;
 
     @Test
-    public void saveToDatabase() throws Exception{
-        Assertions.assertEquals(2, service.parseFileAndSaveRecords(FILE_NAME).size());
+    public void saveToDatabase() throws Exception {
+        Assertions.assertEquals(2, service.parseFileAndSaveRecords(XML_FILE_NAME).size());
     }
+
+    @Test
+    public void validateXmlSchema() throws Exception {
+        Assertions.assertTrue(service.isValidXmlFile(XML_FILE_NAME, SCHEMA_FILE_NAME));
+    }
+
 }
